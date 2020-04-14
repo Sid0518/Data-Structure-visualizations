4527791cffb7a5017f7eb48498ac1b24512748b8
Siddharth Shekhar <30648642+Sid0518@users.noreply.github.com>
2020-04-14 17:25:52 +0530

    Deleted redundant files
    Initially there were two independent projects each for the AVL and RB Tree data structures. Now that they have been combined into a single web application, the old independent versions have been discarded

--- node.js
+++ node.js
@@ -1,184 +1,68 @@
-class Tree {
-    constructor()
-    { this.root = null; }
-
-    addNode(value) {
-        let temp = this.insert(this.root, value);
-        this.root = temp[0];
-
-        return temp[1];
-    }
-
-    insert(node, value, pos = 0) {
-        if(node == null) {
-            let new_node = new Node(value, pos);
-            return [new_node, [new_node]];
-        }
-
-        let nodes = [];
-        if (value < node.value) {
-            let temp = this.insert(node.left, value, 2*pos + 1);
-            node.left = temp[0];
-            nodes = temp[1];
-        }
-        else if(value > node.value) {
-            let temp = this.insert(node.right, value, 2*pos + 2);
-            node.right = temp[0];
-            nodes = temp[1];
-        }
-
-        nodes.push(node);
-        node = this.balance(node);
-
-        return [node, nodes];
-    }
-
-    balance(node) {
-        let delta = node.heightDifference();
-        if (delta > 1) {
-            delta = node.left.heightDifference();
-
-            if(delta > 0)
-                node = this.rightRotate(node);
-            else
-                node = this.leftRightRotate(node);
-        }
-
-        if(delta < -1) {
-            delta = node.right.heightDifference();
-
-            if(delta < 0)
-                node = this.leftRotate(node);
-            else
-                node = this.rightLeftRotate(node);
-        }
-
-        node.updateHeight();
-        return node;
-    }
-
-    leftOf(pos)
-    { return 2*pos + 1; }
-
-    rightOf(pos)
-    { return 2*pos + 2; }
-
-    parentOf(pos)
-    { return floor((pos - 1)/2); }
-
-    rightRotate(x) {
-        let y = x.left;
-        x.left = y.right;
-        y.right = x;
-
-        x.updateHeight();
-        y.updateHeight();
-
-        y.updatePosition(this.parentOf(y.pos));
-
-        return y;
-    }
-
-    leftRightRotate(x) {
-        x.left = this.leftRotate(x.left);
-        return this.rightRotate(x);
-    }
-
-    leftRotate(x) {
-      let y = x.right;
-      x.right = y.left;
-      y.left = x;
-
-      x.updateHeight();
-      y.updateHeight();
-
-      y.updatePosition(this.parentOf(y.pos));
-
-      return y;
-    }
-
-    rightLeftRotate(x) {
-        x.right = this.rightRotate(x.right);
-        return this.leftRotate(x);
-    }
-
-    traverse()
-    { this.root.traverse(); }
-
-    display(size)
-    { this.root.display(size); }
-
-    update()
-    { return this.root.update(); }
-}
-
-class Node {
-    constructor(value, pos) {
+class GenericNode {
+    constructor(value) {
         this.value = value;
-        this.pos = pos;
+        this.height = 1;
 
         this.left = null;
         this.right = null;
 
-        this.x = random(width);
-        this.y = -height/10;
+        this.x = (width / 2);
+        this.y = height;
+
+        this.nx = this.x;
+        this.ny = this.y;
 
         this.vx = 0;
         this.vy = 0;
-        this.updatePosition(this.pos);
 
-        this.height = 1;
+        this.begin = 0;
+        this.width = width;
     }
 
     update() {
         this.x += this.vx;
         this.y += this.vy;
 
-        let dx = abs(this.x - positions[this.pos][0]);
-        let dy = abs(this.y - positions[this.pos][1]);
-        if(dx <= 0.005 && dy <= 0.005 ) {
+        let dx = abs(this.nx - this.x);
+        let dy = abs(this.ny - this.y);
+        if (dx < 1 && dy < 1) {
             this.vx = 0;
             this.vy = 0;
         }
 
-        let a = 1;
-        let b = 1;
         if (this.left != null)
-            a = this.left.update();
+            this.left.update();
         if (this.right != null)
-            b = this.right.update();
-
-        return a && b && this.vx == 0 && this.vy == 0;
+            this.right.update();
     }
 
-    updatePosition(new_position) {
-        this.pos = new_position;
+    updatePositions(begin, width, nx, ny) {
+        this.begin = begin;
+        this.width = width;
 
-        let target_pos = [width/2, -height/10];
-        if(this.pos < positions.length)
-            target_pos = positions[this.pos];
-        else
-            console.log('Node has exceeded maximum allowed depth');
+        this.nx = nx;
+        this.ny = ny;
 
-        this.vx = (target_pos[0] - this.x) / animationSteps;
-        this.vy = (target_pos[1] - this.y) / animationSteps;
+        this.vx = (this.nx - this.x) / animSteps;
+        this.vy = (this.ny - this.y) / animSteps;
 
-        if (this.left != null)
-            this.left.updatePosition(2*this.pos + 1);
-        if (this.right != null)
-            this.right.updatePosition(2*this.pos + 2);
-    }
+        if (this.left != null) {
+            let cbegin = begin;
+            let cwidth = (width / 2);
+            let cnx = nx - (width / 4);
+            let cny = ny + levelGap;
 
-    heightDifference() {
-        let delta = 0;
+            this.left.updatePositions(cbegin, cwidth, cnx, cny);
+        }
 
-        if (this.left != null)
-            delta += this.left.height;
+        if (this.right != null) {
+            let cbegin = begin + (width / 2);
+            let cwidth = (width / 2);
+            let cnx = nx + (width / 4);
+            let cny = ny + levelGap;
 
-        if (this.right != null)
-          delta -= this.right.height;
-
-        return delta;
+            this.right.updatePositions(cbegin, cwidth, cnx, cny);
+        }
     }
 
     updateHeight() {
@@ -192,28 +76,79 @@ class Node {
         this.height = 1 + height;
     }
 
+    print()
+    {   console.log('GenericTree:', this.value);  }
+
     traverse() {
         if (this.left != null)
             this.left.traverse();
 
-        print(this.value);
+        this.print();
 
         if (this.right != null)
             this.right.traverse();
     }
 
     display(size) {
+        fill(255);
+        stroke(0);
+            circle(this.x, this.y, size);
+
+        fill(0);
+        textSize(size / 2);
+            text(this.value, this.x, this.y);
+    }
+
+    displaySubtree(size) {
+        stroke(0);
         if (this.left != null) {
             line(this.x, this.y, this.left.x, this.left.y);
-            this.left.display(size);
+            this.left.displaySubtree(size);
         }
+
+        stroke(0);
         if (this.right != null) {
             line(this.x, this.y, this.right.x, this.right.y);
-            this.right.display(size);
+            this.right.displaySubtree(size);
         }
+
+        this.display(size);
+    }
+}
+
+class AVLNode extends GenericNode {
+    heightDifference() {
+        let delta = 0;
+
+        if (this.left != null)
+            delta += this.left.height;
+
+        if (this.right != null)
+          delta -= this.right.height;
+
+        return delta;
+    }
+
+    print()
+    {   console.log('AVLNode:', this.value);  }
+}
+
+class RedBlackNode extends GenericNode {
+    constructor(value) {
+        super(value);
+        this.colour = RED;
+    }
+
+    print()
+    {   console.log('RedBlackNode:', this.value, this.colour);  }
+
+    display(size) {
         fill(255);
+        stroke(this.colour);
             circle(this.x, this.y, size);
-        fill(0);
+
+        fill(this.colour);
+        textSize(size / 2);
             text(this.value, this.x, this.y);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,66 +1,107 @@
-let width = 1920;
-let height = 900;
+let AVL = 0;
+let RB = 1;
+let treeType = RB;  // set this to AVL or RB
+
+let width = 1800;
+let height = Math.floor((9 / 16) * width);
 
 let borderX = 50
 let borderY = 50;
 
 let tree = null;
+
 let treeValues = [];
-let valueCount = 50;
-let positions = [];
+let valueCount = 36;
 
-let maxLevel = 1 + Math.floor(Math.log2(valueCount)) + 1;
-let nodeCount = Math.pow(2, maxLevel) - 1;
+let level = 5;
 
-let gap = 5;
-let nodeDiameter = 2 * gap*(width - 2*borderX) / ((gap + 1)*(nodeCount + 1)/2 - 1);
-let xshift = (width - 2*borderX)/4;
-let yshift = (height - 2*borderY) / maxLevel;
+let gap = 2;
+let n = Math.pow(2, level - 1);
+let nodeDiameter = ((width - 2*borderX) - (n - 1) * gap) / n;
+let levelGap = (height - 2*borderY) / (level + 2);
 
-let animationSteps = 60;
-let animationRate = 100;
-let FPS = Math.floor(animationSteps * (animationRate/100));
+let rbegin = borderX;
+let rwidth = (width - 2*borderX);
+let rx = (width / 2);
+let ry = borderY + levelGap;
 
-function calcPos(x, y, shift, i = 0) {
-  if (i >= nodeCount)
-    return;
-
-  positions[i] = [x, y];
-  calcPos(x - shift, y + yshift, shift/2, 2*i + 1);
-  calcPos(x + shift, y + yshift, shift/2, 2*i + 2);
-}
+let FPS = 60;
+let pauseTime = 0.4; // between 0 and 1 second
+let animSteps = FPS * (1 - pauseTime);
 
 function setup() {
-  createCanvas(width, height);
-  background(255, 255, 255);
+    createCanvas(width, height);
+    background(255, 255, 255);
 
-  calcPos(width/2, borderY + 0.5*nodeDiameter, xshift);
+    fill(255);
+    stroke(0, 0, 0);
 
-  fill(255);
-  stroke(0, 0, 0);
+    if (treeType == AVL)
+        tree = new AVLTree(nodeDiameter);
+    else if (treeType == RB)
+        tree =  new RedBlackTree(nodeDiameter);
+    else
+        tree = new GenericTree(nodeDiameter);
 
-  tree = new Tree();
-  for (var i = 0; i < valueCount; i++)
-    treeValues[i] = floor(random(0, 200));
+    for (var i = 0; i < valueCount; i++)
+      treeValues[i] = floor(random(0, 200));
 
-  console.log(treeValues);
+    textAlign(CENTER, CENTER);
 
-  textAlign(CENTER, CENTER);
-  textSize(20);
-
-  frameRate(FPS);
+    frameRate(FPS);
 }
 
 let index = 0;
-let visited_nodes = [];
-let insertion_valid = 1;
+let nodes = [];
+let frame = 0;
 
 function draw() {
-  background(255);
+    background(255);
 
-  if (insertion_valid && index < treeValues.length)
-      visited_nodes = tree.addNode(treeValues[index++]);
+    if (frame % FPS == 0) {
+        let rotation_made = false;
+        let node = null;
 
-  insertion_valid = tree.update();
-  tree.display(nodeDiameter);
+        while (nodes.length > 0) {
+            node = nodes.shift();
+            [node, rotation_made] = tree.applyFix(node);
+
+            if(rotation_made) {
+                if (nodes.length > 0) {
+                    let parent = nodes[0];
+                    if (node.value < parent.value)
+                        parent.left = node;
+                    else
+                        parent.right = node;
+                }
+
+                else
+                    tree.root = node;
+
+                tree.root.updatePositions(rbegin, rwidth, rx, ry);
+                nodes.unshift(node);
+                break;
+            }
+        }
+
+        if(!rotation_made) {
+            if (tree.root != null && tree.root.height > level) {
+                level = tree.root.height;
+
+                nodeDiameter = (nodeDiameter - gap) / 2;
+                levelGap = height / (level + 2);
+                ry = levelGap;
+
+                tree.updateSize(nodeDiameter);
+            }
+
+            if(index < treeValues.length)
+                nodes = tree.addNode(treeValues[index++]);
+        }
+    }
+
+    tree.update();
+    tree.display();
+
+    ++frame;
 }
